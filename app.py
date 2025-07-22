3f3b59e6e5253741e088e0c6d59a954e4e27b2ef
lucasteixeira0 <lucas.systemofdow@gmail.com>
2025-07-22 19:18:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -96,8 +96,8 @@ df_inatividade["Data"] = pd.to_datetime(df_inatividade["Data"])
 
 # Filtro de data global
 st.sidebar.header("📅 Filtro de Período")
-data_inicio = df_prod_efetiva["Data"].min()
-data_fim = df_prod_efetiva["Data"].max()
+data_inicio = df_prod_efetiva["Data"].min().date()
+data_fim = df_prod_efetiva["Data"].max().date()
 data_sel = st.sidebar.date_input("Selecione o intervalo:", [data_inicio, data_fim])
 
 # Aplicar o filtro
