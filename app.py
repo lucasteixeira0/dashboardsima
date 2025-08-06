3a54325fe48eba4e06c1fa2553bf4bed178eff9f
lucasteixeira0 <lucas.systemofdow@gmail.com>
2025-08-06 14:50:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -291,7 +291,7 @@ if st.session_state["page"] == "gestao":
             df_prod_efetiva = df_prod_efetiva[(df_prod_efetiva["Data"] >= ini) & (df_prod_efetiva["Data"] <= fim)]
             df_prod_em_processo = df_prod_em_processo[(df_prod_em_processo["Data"] >= ini) & (df_prod_em_processo["Data"] <= fim)]
             df_inatividade = df_inatividade[(df_inatividade["Data"] >= ini) & (df_inatividade["Data"] <= fim)]
-            df_perdas = df_perdas[(df_perdas["Data"] >= ini) & (df_perdas["Data"] <= fim)]
+            
 
         st.markdown(f"📆 Período selecionado: **{ini.date()} a {fim.date()}**")
 
